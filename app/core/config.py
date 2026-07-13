844c87cbc2e9835024050a855bb042adc5a8cfc3
expertmedia-svg <expertmedia@comstratmedia.com>
2026-07-13 16:56:30 +0000

    First

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,9 @@
 import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "LAFIYA Platform"
